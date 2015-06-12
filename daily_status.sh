228b5f9051d957c746310a802904d2432ca2460e
monkeypaddler <monkeypaddlerpi@gmail.com>
2015-06-12 16:07:09 +0100

    adjust mail msg

--- daily_status.sh
+++ daily_status.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# ver 1.0
+# ver 1.1
 #
 
-DAILY_FILE="/home/dad/scripts/daily_status.out"
+DAILY_FILE="/home/dad/io_scripts/daily_status.out"
+SUBJECT="io daily status report"
 TODAY=$(date)
 HOST=$(hostname)
 
+echo $TODAY > $DAILY_FILE
+echo "$HOST is alive!" >> $DAILY_FILE
 
-mail -s "Testing 123" phil.janes@gmail.com <<'EOF'
-Hello World
-===========
-EOF
+cat $DAILY_FILE | mail -s "$SUBJECT" phil.janes@gmail.com 
