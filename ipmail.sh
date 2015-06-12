44d8beb3e1686a2016c1b044ca5b8a31c9e06735
monkeypaddler <monkeypaddlerpi@gmail.com>
2015-06-12 16:26:01 +0100

    fix script

--- ipmail.sh
+++ ipmail.sh
@@ -13,7 +13,7 @@ echo "Firing ipmail.sh script...`date`" >> $LOGFILE
 ip1=""
 ip2=""
 
-read ip1 < /home/dad/scripts/ip.txt
+read ip1 < /home/dad/io_scripts/ip.txt
 ip2=$(wget -qO- ifconfig.me/ip)
 
 
@@ -22,7 +22,7 @@ if [ "$ip1" = "$ip2" ]
 then
   exit
 else
-  echo "$ip2" > /home/dad/scripts/ip.txt
+  echo "$ip2" > /home/dad/io_scripts/ip.txt
   echo "$ip2" | mail -s "$SUBJ" $EMAIL
   exit
 fi
