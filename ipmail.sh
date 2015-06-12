0693e13862ed16f1206aef3237fbb3d6cdf840eb
monkeypaddler <monkeypaddlerpi@gmail.com>
2015-06-12 13:55:26 +0100

    initial commit

---
+++ ipmail.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SUBJ="pumpkin IP has changed"
+EMAIL="phil.janes@gmail.com"
+LOGFILE=/home/dad/scripts/ipmail.log
+
+echo "Firing ipmail.sh script...`date`" >> $LOGFILE
+
+ip1=""
+ip2=""
+
+read ip1 < /home/dad/scripts/ip.txt
+ip2=$(wget -qO- ifconfig.me/ip)
+
+
+
+if [ "$ip1" = "$ip2" ]
+then
+  exit
+else
+  echo "$ip2" > /home/dad/scripts/ip.txt
+  echo "$ip2" | mail -s "$SUBJ" $EMAIL
+  exit
+fi
